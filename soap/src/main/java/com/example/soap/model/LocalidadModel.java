8f15f7e1daa0ac3dc804af33c52dd2cb6a2de706
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-27 18:00:18 -0300

    Create LocalidadModel.java

---
+++ soap/src/main/java/com/example/soap/model/LocalidadModel.java
@@ -0,0 +1,23 @@
+package com.example.soap.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="localidades")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalidadModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    @ManyToOne
+    private ProvinciaModel provincia;
+}
