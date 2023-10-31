cb627ac3dfbf7b312fd9658dd4ca988d2823eeab
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-30 21:33:10 -0300

    Update ProvinciaEndpoint.java

--- soap/src/main/java/com/example/soap/endpoint/ProvinciaEndpoint.java
+++ soap/src/main/java/com/example/soap/endpoint/ProvinciaEndpoint.java
@@ -1,10 +1,11 @@
 package com.example.soap.endpoint;
 
+import com.example.soap.converter.LocalidadConverter;
 import com.example.soap.converter.ProvinciaConverter;
-import com.example.soap.gen.GetProvinciasRequest;
-import com.example.soap.gen.GetProvinciasResponse;
-import com.example.soap.gen.Provincia;
+import com.example.soap.gen.*;
+import com.example.soap.model.LocalidadModel;
 import com.example.soap.model.ProvinciaModel;
+import com.example.soap.repository.LocalidadRepository;
 import com.example.soap.repository.ProvinciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -22,6 +23,10 @@ public class ProvinciaEndpoint {
     private ProvinciaRepository provinciaRepository;
     @Autowired
     private ProvinciaConverter provinciaConverter;
+    @Autowired
+    private LocalidadRepository localidadRepo;
+    @Autowired
+    private LocalidadConverter localidadConverter;
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProvinciasRequest")
     @ResponsePayload
@@ -30,6 +35,17 @@ public class ProvinciaEndpoint {
         List<ProvinciaModel> models = provinciaRepository.findAll();
         List<Provincia> provincias=provinciaConverter.modelsToProvincias(models);
         response.getProvincias().addAll(provincias);
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getLocalidadesRequest")
+    @ResponsePayload
+    public GetLocalidadesResponse getLocalidadesByProvincia(@RequestPayload GetLocalidadesRequest request) {
+        GetLocalidadesResponse response= new GetLocalidadesResponse();
+        List<LocalidadModel> modelsLocalidad=localidadRepo
+                .findByProvincia(provinciaConverter.provinciaToProvinciaModel(request.getProvincia()));
+        List<Localidad> localidades=localidadConverter.mapToLocalidades(modelsLocalidad);
+        response.getLocalidades().addAll(localidades);
         return response;
     }
 }
