628424262942b1222550731ed1d595b75fdf1a1b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-30 21:33:15 -0300

    Update ProvinciaRepository.java

--- soap/src/main/java/com/example/soap/repository/ProvinciaRepository.java
+++ soap/src/main/java/com/example/soap/repository/ProvinciaRepository.java
@@ -3,8 +3,6 @@ package com.example.soap.repository;
 import com.example.soap.model.ProvinciaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProvinciaRepository extends JpaRepository<ProvinciaModel,Long> {
 
 }
